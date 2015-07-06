fcb5ec0eca00b05fda0a6fb961f06ee10cd8a117
Alex Chambers <alexchambers91@gmail.com>
2015-07-06 16:47:56 -0700

    Created file: Project1.java
    Created a class skeleton: the main method body is empty.

---
+++ Project1.java
@@ -0,0 +1,11 @@
+/*
+Author(s): Alex Chambers & Rawya Husain
+Project: project1
+Description: An application that prints "Hello World" to the console, and the
+names of every author.
+*/
+
+public static void main(String args[])
+{
+    //Program starts here!
+}
