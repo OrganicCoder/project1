a49ba9e397ed6e701df4164ad02eb0af1e2644ab
Alex Chambers <alexchambers91@gmail.com>
2015-07-07 11:25:20 -0700

    Properly created Project1 class
    + Placed the main method inside of the class Project1
    * Clarified a comment
    Works with Eclipse from work computer!

--- Project1.java
+++ Project1.java
@@ -2,10 +2,12 @@
 Author(s): Alex Chambers & Rawya Husain
 Project: project1
 Description: An application that prints "Hello World" to the console, and the
-names of every author.
+names of every author of this program.
 */
 
-public static void main(String args[])
-{
-    //Program starts here!
+public class Project1 {
+	public static void main(String args[])
+	{
+	    //Program starts here!
+	}
 }
