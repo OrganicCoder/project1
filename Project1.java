ab4c51b4433010067bb0763cdc4c2b52e20fd92d
RawK <farraw33@users.noreply.github.com>
2015-07-10 01:09:01 -0700

    Updated Introduction
    1. Adjusted 'main' parameter
    2. Added greeting message
    3. Added member name

--- Project1.java
+++ Project1.java
@@ -5,9 +5,19 @@ Description: An application that prints "Hello World" to the console, and the
 names of every author of this program.
 */
 
+package intro;
+
 public class Project1 {
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
 	    //Program starts here!
+	    // Added greeting message 
+	    System.out.println("Hello World!");
+	    // Added name
+	    System.out.println("My name is Rawya Husain.");
+	    
+	    //...
+	
+		
 	}
 }
